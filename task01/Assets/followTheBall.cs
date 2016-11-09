164c6f84b85bd8fdb7ef1bec3b9a8db9972b3179
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2016-11-09 10:17:38 +0600

    first commit/first task

---
+++ task01/Assets/followTheBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followTheBall : MonoBehaviour {
+	public GameObject ball;
+	public float rotationSpeed;
+	public Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject gameObject = ball;
+		transform.position += transform.forward *
+		Time.deltaTime * rotationSpeed;
+	}
+}
+
---
+++ task01/README.md
@@ -0,0 +1,2 @@
+# gameDev
+Here you can find some of my home assignments for Game Development class
