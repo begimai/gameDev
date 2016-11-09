d9ecbd2f6fbd4c950ba46fddac20c8a5000b8542
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2016-11-09 11:14:54 +0600

    setting up environment
    setting up environment

---
+++ task02/Assets/ballBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballBehavior : MonoBehaviour {
+	public float ForceScale = 10.0f;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb.GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 force = new Vector3 (vertical, 0.0f, -horizontal);
+		force = force * ForceScale;
+
+		rb.AddForce (force);	
+	}
+}
