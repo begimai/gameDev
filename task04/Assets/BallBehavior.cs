518f8865d971b6d43c107348470ec83f2bdd35ff
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2016-11-10 16:49:58 +0600

    task04 adding previous environment
    task04 adding previous environment

---
+++ task04/Assets/BallBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour {
+
+	public float IntitialAngle = Random.Range(-75.0f, 75.0f);
+	public float InputForceScale =  50.0f;
+	public AudioSource audioSource;
+	public AudioClip audioForWall;
+	public AudioClip audioForPedal;
+	public Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody.GetComponent<Rigidbody> ();
+		Vector3 force = Quaternion.Euler(0, IntitialAngle, 0) * Vector3.forward;
+		force = force * InputForceScale;
+		rigidBody.AddForce (force);
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject gameObject = collision.gameObject;
+		if (gameObject.CompareTag ("pedal")) {
+			audioSource.PlayOneShot (audioForPedal);
+		} else if (gameObject.CompareTag ("wall")) {
+			audioSource.PlayOneShot (audioForWall);
+		}
+	} 
+}
+
---
+++ task04/Assets/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class GameController : MonoBehaviour {
+
+	public GameObject ballTemp;
+
+	public Text firstPlayerScore;
+	public Text secondPlayerScore;
+
+	private int firstPlayerScoreCounter;
+	private int secondPlayerScoreCounter;
+
+	void OnTriggerExit(Collider other) {
+		GameObject gameObject = other.gameObject;
+
+		if (gameObject.CompareTag ("ball")) {
+			GameObject ball = gameObject;
+
+			bool isToTheLeft = ball.transform.position.z < transform.position.z;
+
+			if (isToTheLeft) {
+				++firstPlayerScoreCounter;
+				firstPlayerScore.text = firstPlayerScoreCounter.ToString();
+			} else {
+				++secondPlayerScoreCounter;
+				secondPlayerScore.text = secondPlayerScoreCounter.ToString();
+			}
+
+			Destroy (ball);
+			BallBehavior newBall = Instantiate (ballTemp).GetComponent<BallBehavior> ();
+
+			if (isToTheLeft) {
+				newBall.IntitialAngle = Random.Range(-75.0f, 75.0f);
+			} else {
+				newBall.IntitialAngle = Random.Range(-75.0f, 75.0f) - 180;
+			}
+		}
+	}
+}
+
